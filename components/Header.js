e639cac32428d54aacdc1af5ace6b705795782f4
Filipe Moreno <y8.filipe@gmail.com>
2022-01-11 15:15:22 -0300

    change: 404 page refactored. Now responsive

--- components/Header.js
+++ components/Header.js
@@ -152,39 +152,39 @@ export default function Header({ online }) {
           </Link>
         </nav>
       </div> */}
-      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b-lg dark:bg-purple-600 dark:border-b-4 dark:border-purple-700">
-        <div class="container flex flex-wrap justify-between items-center mx-auto">
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b-lg dark:bg-purple-600 dark:border-b-4 dark:border-purple-700">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
           {/* <a href="#" class="flex">
             <img class="w-10 h-auto p-1 mr-2" src="/img/logos/rounded-color.png" alt="Logo Battle!" />
               <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Battle!</span>
           </a> */}
-          <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-300 dark:hover:bg-purple-700 dark:focus:ring-purple-600" aria-controls="mobile-menu-2" aria-expanded="false">
-            <span class="sr-only">Abrir menu</span>
-            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-300 dark:hover:bg-purple-700 dark:focus:ring-purple-600" aria-controls="mobile-menu-2" aria-expanded="false">
+            <span className="sr-only">Abrir menu</span>
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </button>
-          <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
               <li>
-                <a href="/" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black" aria-current="page"><div className="flex flex-row items-center"><FaHome className='mr-1' />INÍCIO</div></a>
+                <a href="/" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black" aria-current="page"><div className="flex flex-row items-center"><FaHome className='mr-1' />INÍCIO</div></a>
               </li>
               <li>
-                <a href="/loja" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaShoppingCart className='mr-1' />LOJA</div></a>
+                <a href="/loja" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaShoppingCart className='mr-1' />LOJA</div></a>
               </li>
               <li>
-              <a href="/discord" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaDiscord className='mr-1' />DISCORD</div></a>
+              <a href="/discord" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaDiscord className='mr-1' />DISCORD</div></a>
               </li>
               <li>
-                <a href="/equipe" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaUsers className='mr-1' />EQUIPE</div></a>
+                <a href="/equipe" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaUsers className='mr-1' />EQUIPE</div></a>
               </li>
               <li>
-                <a href="/punicoes" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaBan className='mr-1' />PUNIÇÕES</div></a>
+                <a href="/punicoes" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaBan className='mr-1' />PUNIÇÕES</div></a>
               </li>
               <li>
-                <a href="/changelog" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaFile className='mr-1' />CHANGELOG</div></a>
+                <a href="/changelog" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaFile className='mr-1' />CHANGELOG</div></a>
               </li>
               <li>
-                <a href="/entrar" class="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaSignInAlt className='mr-1' />ENTRAR</div></a>
+                <a href="/conta" className="block py-2 pr-4 pl-3 text-white font-bold rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-black dark:hover:bg-opacity-40 dark:hover:text-white dark:border-black"><div className="flex flex-row items-center"><FaSignInAlt className='mr-1' />ENTRAR</div></a>
               </li>
             </ul>
           </div>
--- pages/404.js
+++ pages/404.js
@@ -11,8 +11,30 @@ export default function Custom404() {
     <>
       <Header />
       <title>404 - Página não encontrada | Rede Battle</title>
-      <div className="flex items-center justify-between pt-10 pl-60 pr-60">
-        <div>
+      <div className="flex items-center justify-center p-8">
+        <div className="p-6 max-w-3xl bg-white rounded-lg border-gray-200 shadow-md dark:bg-dark2 dark:border-b-4 dark:border-black">
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <h1 className="text-9xl font-bold text-gray-300 text-center">
+              404
+            </h1>
+          </motion.div>
+          <h1 className="text-3xl text-gray-300 font-medium text-center">
+            Página não encontrada
+          </h1>
+          <h1 className="text-gray-300 text-center p-2">
+            A página que você procura não existe ou não está disponível no
+            momento.
+          </h1>
+          <a href='/'>
+            <motion.div className='flex justify-center items-center p-2' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+              <button className="flex flex-row items-center justify-center bg-purple-600 border-b-4 border-purple-700 hover:bg-purple-500 hover:border-purple-500 h-10 w-40 sm:text-sm font-medium text-white">
+                Voltar ao início  <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+              </button>
+            </motion.div>
+          </a>
+      </div>
+
+        {/* <div className='sm:flex sm:flex-col w-a'>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
             <h1 className="text-9xl font-bold text-gray-300 text-center">
               404
@@ -25,7 +47,7 @@ export default function Custom404() {
             A página que você procura não existe ou não está disponível no
             momento.
           </h1>
-          <center>
+          <div className='flex items-center justify-center mt-4'>
             <br />
             <Link href="/">
               <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
@@ -34,13 +56,13 @@ export default function Custom404() {
                 </button>
               </motion.div>
             </Link>
-          </center>
-        </div>
-        <div>
+          </div>
+        </div> */}
+        {/* <div className=''>
           <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
             <motion.img src="/img/error404.png" alt="logo" width="300" />
           </motion.div>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </>
