96e749c1d5099c9fa4cdd633f5468448e1e2fdf0
Filipe Moreno <y8.filipe@gmail.com>
2022-01-11 13:03:30 -0300

    change: responsive navbar

--- components/Header.js
+++ components/Header.js
@@ -115,7 +115,7 @@ export default function Header({ online }) {
         </div>
       </div>
 
-      <div className="flex justify-center w-full md:justify-between py-2 px-20 md:py-4 items-center bg-purple-600 border-b-4 border-purple-700 ">
+      {/* <div className="flex justify-center w-full md:justify-between py-2 px-20 md:py-4 items-center bg-purple-600 border-b-4 border-purple-700 sm:hidden">
         <nav className="space-x-8 text-1xl text-white font-semibold ">
           <Link href="/">
             <a className=" hover:text-purple-300 inline-flex items-center tracking-tight">
@@ -155,7 +155,45 @@ export default function Header({ online }) {
             </a>
           </Link>
         </nav>
-      </div>
+      </div> */}
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded-b-lg dark:bg-purple-600 dark:border-b-4 dark:border-purple-700">
+        <div class="container flex flex-wrap justify-between items-center mx-auto">
+          {/* <a href="#" class="flex">
+            <img class="w-10 h-auto p-1 mr-2" src="/img/logos/rounded-color.png" alt="Logo Battle!" />
+              <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">Battle!</span>
+          </a> */}
+          <button data-collapse-toggle="mobile-menu" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-300 dark:hover:bg-purple-700 dark:focus:ring-purple-600" aria-controls="mobile-menu-2" aria-expanded="false">
+            <span class="sr-only">Abrir menu</span>
+            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+          <div class="hidden w-full md:block md:w-auto" id="mobile-menu">
+            <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page"><div className="flex flex-row items-center"><FaHome className='mr-1' />INÍCIO</div></a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaShoppingCart className='mr-1' />LOJA</div></a>
+              </li>
+              <li>
+              <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaDiscord className='mr-1' />DISCORD</div></a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaUsers className='mr-1' />EQUIPE</div></a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaBan className='mr-1' />PUNIÇÕES</div></a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaFile className='mr-1' />CHANGELOG</div></a>
+              </li>
+              <li>
+                <a href="#" class="block py-2 pr-4 pl-3 text-white font-bold bg-blue-700 rounded md:bg-transparent md:text-gray-300 md:p-0 dark:text-gray-300 md:dark:hover:text-white md:dark:hover:bg-black md:dark:hover:bg-opacity-20 md:p-1 md:dark:hover:p-1 dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"><div className="flex flex-row items-center"><FaSignInAlt className='mr-1' />ENTRAR</div></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <CookieConsent
         location="bottom"
         buttonText="Entendi"
