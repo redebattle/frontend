0a27ff0d104d11df3d807b28bb9a1125893b7d9d
Filipe Moreno <y8.filipe@gmail.com>
2022-01-12 00:40:06 -0300

    changes: index pagination

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,62 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import IndexSidebar from '../components/Sidebars/IndexSidebar'
 import PostComponent from '../components/Posts'
+import { array, arrayOf } from 'prop-types'
+import ReactPaginate from 'react-paginate'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+
+const Pagination = styled(ReactPaginate).attrs({
+  activeClassName: 'active',
+})`
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: row;
+  margin: 1rem;
+  justify-content: center;
+  list-style-type: none;
+  padding: 0 6rem;
+  li a {
+    border-radius: 7px;
+    margin: 0.6rem;
+    padding: 0.1rem 1rem;
+    border: gray 1px solid;
+    cursor: pointer;
+  }
+  li.previous a,
+  li.next a,
+  li.break a {
+    border-color: white;
+  }
+  li.active a {
+    background-color: #8b5cf6;
+    border-color: transparent;
+    color: white;
+    min-width: 32px;
+  }
+  li.disabled a {
+    color: grey;
+  }
+  li.disable,
+  li.disabled a {
+    cursor: default;
+  }
+  @media only screen and (max-width: 500px) {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    li a {
+      border-radius: 7px;
+      justify-content: space-between;
+      border: gray 1px solid;
+      cursor: pointer;
+    }
+  }
+`;
 
 export default function Home({ posts, postsInfo, query, error, manutencao }) {
+
+  const router = useRouter()
 
   if (error) {
     return (
@@ -23,18 +77,27 @@ export default function Home({ posts, postsInfo, query, error, manutencao }) {
   }
 
   let page = query.pagina || 1
-  const total = postsInfo.total / postsInfo.quantidade
+  const totalPage = postsInfo.total / 10
 
+  const pagginationHandler = async (pagina) => {
+    const currentPath = router.pathname;
+    const currentQuery = { ...router.query };
+    currentQuery.pagina = pagina.selected + 1;
 
-  const handleNextPage = page => {
-    let next = page++
-    return next
-  }
+    await router.push({
+        pathname: currentPath,
+        query: currentQuery,
+    });
+
+};
+
   return (
     <>
       <Header />
       <div className="INDEX">
         <title>Rede Battle</title>
+        {/* METADATA */}
+        <Metadata title={'Rede Battle'} description={'Site oficial da Rede Battle!'} imgURL={'https://redebattle.com.br/img/last-purchases-bg.jpg'} />
         <div className="flex justify-center p-8 lg:flex-row sm:flex-col">
           <div className='w-full sm:mb-2'>
             {(postsInfo.obs.rows.length === 0 && (
@@ -68,7 +131,18 @@ export default function Home({ posts, postsInfo, query, error, manutencao }) {
                     acessos={post.acessos}
                   />
                 )
-              })}
+              })
+            }
+            {totalPage > 1 &&
+              <div>
+                <Pagination
+                  pageCount={totalPage}
+                  onPageChange={pagginationHandler}
+                  breakLabel="..."
+                  previousLabel="«"
+                  nextLabel="»"
+                />
+            </div>}
           </div>
           <div className=''>
             <IndexSidebar />
@@ -152,7 +226,7 @@ export default function Home({ posts, postsInfo, query, error, manutencao }) {
 
 export async function getServerSideProps({ query }) {
   try {
-    const page = query.pagina || 1
+    let { pagina } = query
     // const posts = await api
     //   .get(`/postagens/list?page=${page}&itens=1&sort=createdAt&order=desc`)
     //   .then(res => res.data.obs.rows)
@@ -160,7 +234,7 @@ export async function getServerSideProps({ query }) {
     //     console.log('Ocorreu um erro ao acessar a API de getAllPosts', e)
     //   })
     const postsInfo = await api
-      .get(`/postagens/list?page=${page}&itens=10&sort=createdAt&order=desc`)
+      .get(`/postagens/list?page=${pagina ? pagina : 1}&itens=10&sort=createdAt&order=desc`)
       .then(res => res.data)
       .catch(e => {
         console.log('Ocorreu um erro ao acessar a API de getAllPosts', e)
